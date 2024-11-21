012dbd713ce9f6bffe3f1c0a3ccc36eb9fa56566
GertNingbers <97081115@st.deltion.nl>
2024-11-21 12:18:00 +0100

    V2 JSONB

--- JS-32b/API-jsonb.js
+++ JS-32b/API-jsonb.js
@@ -1,4 +1,3 @@
-// Bewaar de url waarop de API staat in een variabele
 let url = "https://std.stegion.nl/cs_codebase/js31_api_base/users.json";
 let urlBier = "https://std.stegion.nl/api_bier/api.php?action=getBeers";
 
@@ -7,12 +6,14 @@ let urlBier = "https://std.stegion.nl/api_bier/api.php?action=getBeers";
 function fGetHtml(bier) {                  
     var htmlTable = "<table class='tblA txt_sm'>";
     htmlTable += "<tr>";                    // plaats de row met headers van de tabel
+        htmlTable += "<th>nr</th>";
         htmlTable += "<th>naam</th>";
         htmlTable += "<th>gisting</th>";
         htmlTable += "<th>percentage</th>";
     htmlTable += "</tr>";
     for (var i = 0; i < bier.length; i++) { // loop door alle users plaats de row met headers van de tabel
         htmlTable += "<tr>";
+            htmlTable += "<td>" + (i + 1) + "</td>";
             htmlTable += "<td>" + bier[i].naam + "</td>";   // haal van de array nr i van de users, het object key "name" op
             htmlTable += "<td>" + bier[i].gisting + "</td>";    // haal van de array nr i van de users, het object key "dob" (date of birth) op
             htmlTable += "<td>" + bier[i].perc + "</td>";  // haal van de array het email adres op
@@ -32,7 +33,7 @@ function getHtmlFromUrl(url) {  // haal dat van URL binnen, verwerkt dit in een 
             console.log("json = ", json);               // geef ter controle in de console de opgehaalde JSON weer. 
             console.log("bier = ", json.data);        // de json bevat users en offices, laat hier alleen de users zien 
             let htmlUsersTable = fGetHtml(json.data);  // fGetHtml geeft een variabele terug met de HTML-table, sla de op in "htmlUsersTable"
-            document.getElementById("out_tableA").innerHTML = htmlUsersTable;
+            document.getElementById("output1").innerHTML = htmlUsersTable;
         });
 }
 /*   ----- stop ------------ 1 "geneste" function om data op te halen, met fGetHtml() de HTML ophalen en die plaatsen op de webpagina -------- */
@@ -40,29 +41,32 @@ function getHtmlFromUrl(url) {  // haal dat van URL binnen, verwerkt dit in een 
 
 
 /*   ----- start ----------- 2 functions die eerst de data ophalen, wachten tot die binnen is (met aSync) ------------------------------------ */
-function fGetData (url) {  // haalt de data op van de url (API) en geeft met "return json" de data terug
-    //console.log("in fFetchDataAsync. url = ", url)
-    return fetch(url)
-        .then(data => {
-            return data.json();
-        })
-        .then(json => {
-            console.log("Users in fFetchDataAsync:", json); // geef ter controle in de console de opgehaalde JSON weer. 
-            return json
-        });
+async function fGetData(url) {
+    const cachedData = localStorage.getItem(url);
+    if (cachedData) {
+        return JSON.parse(cachedData);
+    }
+    
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const json = await response.json();
+    localStorage.setItem(url, JSON.stringify(json)); // Caching
+    return json;
 }
 
 async function getHtmlFromUrlAsync(url) {  // is een asynchrone function (lees hieronder waarom)
-    //console.log("url = ", url)
+    console.log("url = ", url)
     let myJson = await fGetData(url); // ophalen van fGetData() duurt even. 
     // Om te voorkomen dat het script direct naar de volgende regel doorgaat voordat de data binnen is, zeg je dat hij moet wachten op de data met "await"
     // Als je await wil gebruiken in een function moet je deze "asynchroon" maken door voor functio, "async" te zetten
     console.log("myJson = ", myJson);
     let html = fGetHtml(myJson.users);
-    document.getElementById("out_tableB").innerHTML = html;
+    document.getElementById("output2").innerHTML = html;
 }
 /*   ----- start ----------- 2 functions die eerst de data ophalen, wachten tot die binnen is (met aSync) ------------------------------------ */
 
 // voer de "geneste" methode uit, Ã³f voor de nettere, async methode bij opstarten meteen uit (ze beschrijven beide dezelfde "out_table"-div)
 getHtmlFromUrl(urlBier);        
-//getHtmlFromUrlAsync(url); 
+getHtmlFromUrlAsync(url); 
